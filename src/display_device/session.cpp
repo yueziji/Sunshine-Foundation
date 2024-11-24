b64609a42c57deb9a649dc555e874d14ad71e858
yueziji <21022131+yueziji@users.noreply.github.com>
2024-11-25 00:04:31 +0800

    Update session.cpp

--- src/display_device/session.cpp
+++ src/display_device/session.cpp
@@ -177,7 +177,7 @@ namespace display_device {
 
     if (settings.is_changing_settings_going_to_fail()) {
       BOOST_LOG(debug) << "this is before timer session:" << session.width << "x" << session.height << "@" << session.fps;
-      timer->setup_timer([this, &config, session_copy = *session, &session, &is_reconfigure, config_copy = *parsed_config]() {
+      timer->setup_timer([this, &config, session_copy = session, &session, &is_reconfigure, config_copy = *parsed_config]() {
         BOOST_LOG(debug) << "this is before while cycle session:" << session.width << "x" << session.height << "@" << session.fps;
         while (true) {
           {
